7f519d0e1b70738cf2e5d8c566877de99541c1a3
Prasanna TG <tgprasanna12@gmail.com>
2025-07-14 08:13:05 +0530

    weight and unit added

--- src/components/FruitList.js
+++ src/components/FruitList.js
@@ -1,14 +1,40 @@
 // :white_tick: :two: FruitList.js — Pick fruits
 
-import React from 'react'
+import React, { useState } from 'react'
 export default function FruitList({ fruits, addToCart }) {
+
+  const [weight, setweight] = useState([]);
+
+  const handleChange = (index, input) => {
+    const newInput = [...weight]
+    newInput[index] = input.target.value;
+    fruits[index].weight = input.target.value
+    setweight(newInput)
+    console.log("FRUITS", fruits)
+  }
+
   return (
     <div>
       <h2>Fruits</h2>
       <ul className='fruitsul'>
-        {fruits.map(fruit => (
+        {fruits.map((fruit, index) => (
           <li className='fruitsli' key={fruit.id}>
             <div>{fruit.name} — ₹{fruit.price}</div>
+            <div className='divWeightInputUnit'>
+               <div>
+              <input  key={index} type="text" className="txtWeight"
+                value={weight[index] || ''}
+              onChange={(e)=> handleChange(index, e, fruits)}
+              placeholder={'index - ' +index}/>
+            </div>
+           
+            <div>
+              <select type="">
+                <option>gm</option>
+                <option>kg</option>
+              </select>
+            </div>
+            </div>
             <div style={{marginTop:"10px"}}><button onClick={() => addToCart(fruit)}>Add</button></div>
           </li>
         ))}
