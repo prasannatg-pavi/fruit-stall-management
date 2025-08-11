2793fe4635f779bba7c7ae6993f5444bc4105670
Prasanna TG <tgprasanna12@gmail.com>
2025-08-12 00:51:34 +0530

    pavi stall push

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,11 +1,11 @@
 import { createClient } from "@supabase/supabase-js"
 
 // Default: Prasanna space
-const supabaseUrl = 'https://tvllicmyzhgdteboasho.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR2bGxpY215emhnZHRlYm9hc2hvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTE5OTQzMzQsImV4cCI6MjA2NzU3MDMzNH0.zHzhbaSqTGJg_9q3qoAnbFEQ1aP21YM2OxyfCI-A9t4'
+// const supabaseUrl = 'https://tvllicmyzhgdteboasho.supabase.co'
+// const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR2bGxpY215emhnZHRlYm9hc2hvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTE5OTQzMzQsImV4cCI6MjA2NzU3MDMzNH0.zHzhbaSqTGJg_9q3qoAnbFEQ1aP21YM2OxyfCI-A9t4'
 
 // Pavithra
-// const supabaseUrl = 'https://eljhbevspjqxvmnrcvbj.supabase.co'
-// const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVsamhiZXZzcGpxeHZtbnJjdmJqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQ2NzYxMzAsImV4cCI6MjA3MDI1MjEzMH0.-uFuQOi9vXZfhAscbOUIYypqSHeRmPWuN5LatAtICAk'
+const supabaseUrl = 'https://eljhbevspjqxvmnrcvbj.supabase.co'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVsamhiZXZzcGpxeHZtbnJjdmJqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQ2NzYxMzAsImV4cCI6MjA3MDI1MjEzMH0.-uFuQOi9vXZfhAscbOUIYypqSHeRmPWuN5LatAtICAk'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
